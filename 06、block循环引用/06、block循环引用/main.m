63885c65b37e45365e01ef9e07b19773d6565055
tens04 <tens04@tens04.local>
2016-08-23 20:15:56 +0800

    block的循环引用

---
+++ 06、block循环引用/06、block循环引用/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  06、block循环引用
+//
+//  Created by tens04 on 16/8/23.
+//  Copyright (c) 2016年 tens04. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
