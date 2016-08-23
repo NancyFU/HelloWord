3b3296834f1982f52c6f08a20b12b98e6ee058e4
tens04 <tens04@tens04.local>
2016-08-23 14:09:59 +0800

    block传值练习：用户注册

---
+++ 02、block传值/02、block传值/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  02、block传值
+//
+//  Created by tens04 on 16/8/23.
+//  Copyright (c) 2016年 tens04. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
