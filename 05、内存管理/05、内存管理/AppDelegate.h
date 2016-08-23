b4f6677129824e014074e40703b14f3d4d05abf7
tens04 <tens04@tens04.local>
2016-08-23 19:10:27 +0800

    block的内存管理

---
+++ 05、内存管理/05、内存管理/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  05、内存管理
+//
+//  Created by kinglinfu on 16/8/23.
+//  Copyright © 2016年 Tens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 05、内存管理/05、内存管理/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  05、内存管理
+//
+//  Created by kinglinfu on 16/8/23.
+//  Copyright © 2016年 Tens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
