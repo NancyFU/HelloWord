ff32bae5a3b8a3c0acdfcb269d5fe7d92fbe7a69
tens04 <tens04@tens04.local>
2016-08-24 16:03:00 +0800

    CALayer的动画

---
+++ 03、CALayer_3D变换/03、CALayer_3D变换Tests/_3_CALayer_3D__Tests.m
@@ -0,0 +1,40 @@
+//
+//  _3_CALayer_3D__Tests.m
+//  03、CALayer_3D变换Tests
+//
+//  Created by tens04 on 16/8/24.
+//  Copyright (c) 2016年 tens04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _3_CALayer_3D__Tests : XCTestCase
+
+@end
+
+@implementation _3_CALayer_3D__Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
