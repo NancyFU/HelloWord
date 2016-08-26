2b1648ec656c718673bb219d9d1f62d8c58a2b3d
tens04 <tens04@tens04.local>
2016-08-26 14:52:21 +0800

    动画的基本变换代码练习

---
+++ 02、CABasicAnimation练习/02、CABasicAnimation练习/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  02、CABasicAnimation练习
+//
+//  Created by tens04 on 16/8/26.
+//  Copyright (c) 2016年 tens04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
